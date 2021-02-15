9244d7ab254cb10dde5f343fa5251c95c2d8fd7a
jazewh <77996432+jazewh@users.noreply.github.com>
2021-02-15 17:15:00 -0500

    exo1

--- exercice.py
+++ exercice.py
@@ -5,9 +5,11 @@
 def order(values: list = None) -> list:
     if values is None:
         # TODO: demander les valeurs ici
-        pass
-
-    return []
+        values=[]
+        for i in range(10):
+            valeur=input("Entrer une donnée ")
+            values.append(valeur)
+    return sorted(values)
 
 
 def anagrams(words: list = None) -> bool:
@@ -48,24 +50,24 @@ def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
     order()
 
-    print(f"On vérifie les anagrammes...")
-    anagrams()
-
-    my_list = [3, 3, 5, 6, 1, 1]
-    print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
-
-    grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
-    best_student = best_grades(grades)
-    print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
-
-    sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
-    frequence(sentence)
-
-    print("On enregistre les recettes...")
-    recipes = get_recipes()
-
-    print("On affiche une recette au choix...")
-    print_recipe(recipes)
+    # print(f"On vérifie les anagrammes...")
+    # anagrams()
+    #
+    # my_list = [3, 3, 5, 6, 1, 1]
+    # print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
+    #
+    # grades = {"Bob": [90, 65, 20], "Alice": [85, 75, 83]}
+    # best_student = best_grades(grades)
+    # print(f"{list(best_student.keys())[0]} a la meilleure moyenne: {list(best_student.values())[0]}")
+    #
+    # sentence = "bonjour, je suis une phrase. je suis compose de beaucoup de lettre. oui oui"
+    # frequence(sentence)
+    #
+    # print("On enregistre les recettes...")
+    # recipes = get_recipes()
+    #
+    # print("On affiche une recette au choix...")
+    # print_recipe(recipes)
 
 
 if __name__ == '__main__':
